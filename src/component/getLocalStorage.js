3d3a88ad54d4be7604775206b30ff8c4b002761b
bab-gam <nikitalav21lin@gmail.com>
2022-07-29 14:14:13 +0300

    add LocalStorage

--- src/component/getLocalStorage.js
+++
@@ -1,8 +0,0 @@
-const getLocalStorage = () => {
-  let posts = localStorage.getItem("list")
-  if (posts) {
-    return (posts = JSON.parse(localStorage.getItem("list")))
-  } else {
-    return []
-  }
-} 
